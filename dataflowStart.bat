1564283154909b464168e5dd704d799540d91238
fabioportieri <fazfazzi@gmail.com>
2023-01-21 11:38:39 +0100

    externalized kafka

--- dataflowStart.bat
+++ dataflowStart.bat
@@ -2,4 +2,4 @@ set DATAFLOW_VERSION=2.10.0-SNAPSHOT&
 set SKIPPER_VERSION=2.9.0-SNAPSHOT&
 set HOST_MOUNT_PATH=%userprofile%\.m2
 set DOCKER_MOUNT_PATH=/root/.m2/
-docker-compose -f docker-compose.yml -f docker-compose-kafka.yml -f docker-compose-postgres.yml -f docker-compose-prometheus.yml up
+docker-compose -f docker-compose.yml -f docker-compose-kafka-ext.yml -f docker-compose-postgres.yml -f docker-compose-prometheus.yml up
---
+++ dataflowStart.sh
@@ -0,0 +1,5 @@
+export DATAFLOW_VERSION=2.10.0-SNAPSHOT
+export SKIPPER_VERSION=2.9.0-SNAPSHOT
+export HOST_MOUNT_PATH=~/.m2
+export DOCKER_MOUNT_PATH=/root/.m2/
+docker-compose -f docker-compose.yml -f docker-compose-kafka-ext.yml -f docker-compose-postgres.yml -f docker-compose-prometheus.yml up
