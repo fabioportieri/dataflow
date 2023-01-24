bae6cd8eca60f5d8b5f75606dfa87c39f195fb47
fabioportieri <fazfazzi@gmail.com>
2023-01-24 09:14:54 +0100

    externalized kafka broker

--- dataflowStart.sh
+++ dataflowStart.sh
@@ -3,3 +3,4 @@ export SKIPPER_VERSION=2.9.0-SNAPSHOT
 export HOST_MOUNT_PATH=~/.m2
 export DOCKER_MOUNT_PATH=/root/.m2/
 docker-compose -f docker-compose.yml -f docker-compose-kafka-ext.yml -f docker-compose-postgres.yml -f docker-compose-prometheus.yml up
+#docker-compose -f docker-compose.yml -f docker-compose-kafka.yml -f docker-compose-postgres.yml -f docker-compose-prometheus.yml up
