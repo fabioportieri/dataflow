1df576cd6db3e04e87a499053af8f3cbafde3c0f
fabioportieri <fazfazzi@gmail.com>
2023-01-30 18:34:25 +0100

    creata rete dataflow

--- dataflowStart.bat
+++ dataflowStart.bat
@@ -2,4 +2,5 @@ set DATAFLOW_VERSION=2.10.0-SNAPSHOT&
 set SKIPPER_VERSION=2.9.0-SNAPSHOT&
 set HOST_MOUNT_PATH=%userprofile%\.m2
 set DOCKER_MOUNT_PATH=/root/.m2/
-docker-compose -f docker-compose.yml -f docker-compose-kafka-ext.yml -f docker-compose-postgres.yml -f docker-compose-prometheus.yml up
+docker-compose -f common.yml -f docker-compose.yml -f docker-compose-kafka-ext.yml -f docker-compose-postgres.yml -f docker-compose-prometheus.yml up
+
